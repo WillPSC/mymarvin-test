85977a301da08d139c81e654a407d7b72829ab13
William <WIlliam.offei@epitech.eu>
2021-04-11 12:31:32 +0200

    push william :)

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
 ENV CASC_JENKINS_CONFIG /var/jenkins_home/casc.yaml
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
+COPY job_dsl.groovy /var/jenkins_home/job_dsl.groovy
 COPY my_marvin.yaml /var/jenkins_home/casc.yaml
--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,7 +1,21 @@
-job('Disk Space Check') {
-  steps {
-    shell('df')
-  }
+folder('Tools') {
+    description('Folder for miscellaneous tools.')
+    job('Tools/clone-repository') {
+        parameters {
+            stringParam('GIT_REPOSITORY_URL', null, 'Git URL of the repository to clone')
+        }
+        steps {
+            shell('git clone ${GIT_REPOSITORY_URL}')
+        }
+        wrappers {
+        preBuildCleanup()
+       }
+    }
+
+    job('Tools/SEED') {
+        parameters {
+            stringParam("GITHUB_NAME", null, 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine)')
+            stringParam("DISPLAY_NAME", null, 'Display name for the job')
+        }
+    }
 }
-
-
